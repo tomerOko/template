69c7b313ff0cce155cfc3dcac9a770dc77947a1b
tomerOko <tomeroko2@gmail.com>
2022-04-18 01:36:16 +0300

    smarter scripts

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,13 +1,53 @@
 #!/bin/sh
 # run from <...>/scripts
-cd ../
-sudo docker build -t run_ts_image . # build
+cd ../ 
+
+# load flags from the run command (bash <script name>.sh --<flag key> <flag value>)
+while getopts i:c:p: flag
+do
+    case "${flag}" in
+        i) imageName=${OPTARG};;
+        c) containerName=${OPTARG};;
+        p) port=${OPTARG};;
+    esac
+done
+
+# assign defult vales to empty flag's variables
+if [ -z "$imageName" ];then
+    imageName="run_ts_image";
+fi
+
+if [ -z "$containerName" ];then
+    containerName="run_ts_container";
+fi
+
+if [ -z "$port" ];then
+    port=3005;
+fi
+
+# look for arguments
+for var in "$@"
+do
+    case "${var}" in
+        no-build) noBuild=1;;
+    esac
+done
+
+
+# dont build image only if the image already exits and 'no-build' flad was provided
+if ([ -n "$(docker images -q $imageName)" ] && [ -n "$noBuild" ]);
+then
+    echo "image is exist and 'no-build' flag is on => not building image $imageName";
+else
+    sudo docker build -t $imageName . # build
+fi
+
 sudo chown -R $USER:$(id -gn $USER) ./* # give permmisions in order to be able to adit the files
-docker container rm -f run_ts_container # remove container if allready runing
-docker run -itd --name run_ts_container -p 3005:3000 -v "$(pwd)"/:/app/ run_ts_image # run the container (entrypoint in dockerfile)
-sleep 2 # give the container a two seconds to boot
-docker container ls --filter name=run_ts_container # make sure the container is still runing
-docker logs --follow run_ts_container # connect the shell to the container's logs outpout
+docker container rm -f $containerName # remove container if allready runing
+docker run -itd --name $containerName -p $port:3000 -v "$(pwd)"/:/app/ $imageName # run the container (entrypoint in dockerfile)
+sleep 1 # give the container a second to boot
+docker container ls --filter name=$containerName # make sure the container is actualy runing
+docker logs --follow $containerName # connect the shell to the container's logs outpout
 
 # open new terminal and run :
 # docker container exec -it run_ts_container bash
